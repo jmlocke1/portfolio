5202b707fba4a945dfa579a552d9cc1bcb045369
Josemi Xeon <worldcode.info@gmail.com>
2023-10-21 23:55:22 +0200

    Añadido el fichero regenera.php para crear todos los ficheros de despliegue (.gitignore, clonesub, pull y push)

---
+++ regenera.php
@@ -0,0 +1,247 @@
+<?php
+// Declaraciones de repositorios
+$subdomain = [
+	[
+		'name' => 'DevWebCamp',
+		'repo' => 'git@github.com:jmlocke1/DevWebCamp.git'
+	],
+	[
+		'name' => 'bienesraices',
+		'repo' => 'git@github.com:jmlocke1/bienesraices.git'
+	],
+	[
+		'name' => 'appsalon',
+		'repo' => 'git@github.com:jmlocke1/appsalon.git'
+	],
+	[
+		'name' => 'uptask',
+		'repo' => 'git@github.com:jmlocke1/uptask.git'
+	]
+];
+$noSubdomain = [
+	[
+		'name' => 'blogdecafe',
+		'repo' => 'git@github.com:jmlocke1/blogdecafe.git'
+	],
+	[
+		'name' => 'frontendstore',
+		'repo' => 'git@github.com:jmlocke1/frontendstore.git'
+	],
+	[
+		'name' => 'rock-edm-festival',
+		'repo' => 'git@github.com:jmlocke1/rock-edm-festival.git'
+	],
+	[
+		'name' => 'tienda-muebles',
+		'repo' => 'git@github.com:jmlocke1/tienda-muebles.git'
+	]
+];
+// Funciones
+function escribe($fichero, $texto) {
+	$filephp = fopen($fichero, "w");
+	fwrite($filephp, $texto);
+	fclose($filephp);
+}
+
+// Fichero .gitignore
+$texto = <<<PRE
+# Copia de seguridad de scripts en linux
+*.sh~
+
+# Repositorios con subdominio
+
+PRE;
+foreach($subdomain as $repo){
+	$texto .= $repo['name'].PHP_EOL;
+}
+$texto .= PHP_EOL.PHP_EOL."# Repositorios sin subdominio".PHP_EOL.PHP_EOL;
+foreach($noSubdomain as $repo){
+	$texto .= $repo['name'].PHP_EOL;
+}
+escribe('.gitignore', $texto);
+
+// Fichero para clonar todos los repositorios
+// Windows
+$texto = "@REM Repositorios con subdominio".PHP_EOL.PHP_EOL;
+
+foreach($subdomain as $repo){
+	$texto .= "git clone {$repo['repo']}".PHP_EOL;
+}
+$texto .= PHP_EOL.PHP_EOL."@REM Repositorios sin subdominio".PHP_EOL.PHP_EOL;
+foreach($noSubdomain as $repo){
+	$texto .= "git clone {$repo['repo']} no-sub/{$repo['name']}".PHP_EOL;
+}
+escribe('clonesub.bat', $texto);
+
+// Linux
+$texto = <<<PRE
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*-
+# Repositorios con subdominio
+PRE;
+foreach($subdomain as $repo){
+	$texto .= "git clone {$repo['repo']}".PHP_EOL;
+}
+$texto .= PHP_EOL.PHP_EOL."# Repositorios sin subdominio".PHP_EOL.PHP_EOL;
+foreach($noSubdomain as $repo){
+	$texto .= "git clone {$repo['repo']} no-sub/{$repo['name']}".PHP_EOL;
+}
+escribe('clonesub.sh', $texto);
+
+// Fichero para obtener los cambios de todos los repositorios
+// incluido el repositorio portfolio
+// Windows
+$texto = <<<PRE
+@REM Primero obtenemos los cambios del repositorio actual
+git pull
+
+@REM Repositorios con subdominio
+
+
+PRE;
+foreach($subdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git pull
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+
+@REM Repositorios sin subdominio
+
+cd no-sub
+
+PRE;
+foreach($noSubdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git pull
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+@REM Volvemos al repositorio raíz
+
+cd ..
+
+PRE;
+escribe('pull.bat', $texto);
+
+// Linux
+$texto = <<<PRE
+# Primero obtenemos los cambios del repositorio actual
+git pull
+
+# Repositorios con subdominio
+
+
+PRE;
+foreach($subdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git pull
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+
+# Repositorios sin subdominio
+
+cd no-sub
+
+PRE;
+foreach($noSubdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git pull
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+# Volvemos al repositorio raíz
+
+cd ..
+
+PRE;
+escribe('pull.sh', $texto);
+
+// Fichero para subir los cambios de todos los repositorios
+// Aunque no es muy recomendable, mejor subir los cambios de uno en uno
+// Windows
+$texto = "@REM Repositorios con subdominio".PHP_EOL.PHP_EOL;
+foreach($subdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git push
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+
+@REM Repositorios sin subdominio
+
+cd no-sub
+
+PRE;
+foreach($noSubdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git push
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+@REM Volvemos al repositorio raíz
+
+cd ..
+
+PRE;
+escribe('push.bat', $texto);
+
+// Linux
+$texto = "# Repositorios con subdominio".PHP_EOL.PHP_EOL;
+foreach($subdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git push
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+
+# Repositorios sin subdominio
+
+cd no-sub
+
+PRE;
+foreach($noSubdomain as $repo){
+	$texto .= <<<PRE
+cd {$repo['name']}
+git push
+cd ..
+
+PRE;
+}
+$texto .= <<<PRE
+
+# Volvemos al repositorio raíz
+
+cd ..
+
+PRE;
+escribe('push.sh', $texto);
