bf975e2764e0ded597d8c5f811f384960b595b04
Josemi Xeon <worldcode.info@gmail.com>
2023-11-04 00:25:12 +0100

    Creada estructura de contenedor en el cuerpo de la página

--- no-sub/index.php
+++ no-sub/index.php
@@ -31,22 +31,35 @@ require_once "../config/Config.php";
 			</div>
 		</div>
 	</header>
-	<h1>Portfolio personal de José Miguel</h1>
 
-	<h2>Proyectos de diseño en CSS.</h2>
-	<p>E-Commerce <a href="/tienda-muebles/">Tienda de Muebles</a> </p>	
-	<p><a href="/blogdecafe/">Blog de café</a>. Blog sobre el café, y cursos</p>
-	<p><a href="/frontendstore">Frontend Store</a>. Tienda de venta de camisetas</p>
-	<p><a href="/rock-edm-festival">Rock EDM Festival</a>. Festival de Rock</p>
-	<p><a href="/tech-pro">TechPro</a>. Ejemplo de Landing Page de venta de una marca de auriculares.</p>
-	<p><a href="/arquitectura-bosque">Arquitectura Bosque</a>. Plantilla de un diseño de casas en el bosque</p>
+	<div class="contenedor">
+		<h1>Portfolio personal de José Miguel</h1>
 
-	<h2>Aplicaciones completas con CSS, PHP, Bases de datos y JavaScript.</h2>
+		<p>En este portfolio muestro mis proyectos personales y prácticas realizadas.</p>
+	</div>
+	
+	<div class="contenedor">
+		<h2>Proyectos de diseño en CSS.</h2>
+		<p>E-Commerce <a href="/tienda-muebles/">Tienda de Muebles</a> </p>	
+		<p><a href="/blogdecafe/">Blog de café</a>. Blog sobre el café, y cursos</p>
+		<p><a href="/frontendstore">Frontend Store</a>. Tienda de venta de camisetas</p>
+		<p><a href="/rock-edm-festival">Rock EDM Festival</a>. Festival de Rock</p>
+		<p><a href="/tech-pro">TechPro</a>. Ejemplo de Landing Page de venta de una marca de auriculares.</p>
+		<p><a href="/arquitectura-bosque">Arquitectura Bosque</a>. Plantilla de un diseño de casas en el bosque</p>
 
-	<p><a href="<?= "https://". Config::DEVWEBCAMP; ?>">DevWebCamp</a>. Proyecto de un Webcamp de conferencias</p>
-	<p><a href="<?= "https://". Config::BIENES_RAICES; ?>">Bienes Raíces</a>. Aplicación de una inmobiliaria, con gestión de propiedades y vendedores</p>
-	<p><a href="<?= "https://". Config::APPSALON; ?>">APPSALON</a>. Gestión de reservas en una peluquería. Se pueden reservar servicios.</p>
-	<p><a href="<?= "https://". Config::UPTASK; ?>">UpTask</a>. Crea y administra tus proyectos. Crea proyectos y tareas para los proyectos. Puedes cambiar las tareas a pendientes o completas.</p>
+
+		<h2>Aplicaciones completas con CSS, PHP, Bases de datos y JavaScript.</h2>
+
+		<p><a href="<?= "https://". Config::DEVWEBCAMP; ?>">DevWebCamp</a>. Proyecto de un Webcamp de conferencias</p>
+		<p><a href="<?= "https://". Config::BIENES_RAICES; ?>">Bienes Raíces</a>. Aplicación de una inmobiliaria, con gestión de propiedades y vendedores</p>
+		<p><a href="<?= "https://". Config::APPSALON; ?>">APPSALON</a>. Gestión de reservas en una peluquería. Se pueden reservar servicios.</p>
+		<p><a href="<?= "https://". Config::UPTASK; ?>">UpTask</a>. Crea y administra tus proyectos. Crea proyectos y tareas para los proyectos. Puedes cambiar las tareas a pendientes o completas.</p>
+
+	</div>
+
+	
+
+	
 
 </body>
 </html>
